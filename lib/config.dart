2faa9f766b145132ea7d76ba777d7d36ac81cdac
LeeZEun <je990420@ajou.ac.kr>
2024-07-19 11:02:54 +0900

    [Refactor] config.dart 파일 내용 수정

--- lib/config.dart
+++ lib/config.dart
@@ -1,14 +1,6 @@
 class Config {
   static const String appName = "TripFlow";
 
-  //Config
-  // static const apiURL = "ec2-43-202-92-163.ap-northeast-2.compute.amazonaws.com:8080";
-  static const apiURL = "127.0.0.1:8443";
-
-  //User Info
-  static const loginAPI = "/user/login";
-  static const signupAPI = "/user/register";
-
   //sample
   static const sampleAPI = "/sample";
 }
