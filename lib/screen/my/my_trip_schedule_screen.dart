5086a04d1a529dcab7af31cfe8186ceee871549a
LeeZEun <je990420@ajou.ac.kr>
2024-07-25 23:13:58 +0900

    [Feat] 일정 상세 Dialog 구현

--- lib/screen/my/my_trip_schedule_screen.dart
+++ lib/screen/my/my_trip_schedule_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../component/dialog/trip_detail_dialog.dart';
 import '../../controller/my_menu_controller.dart';
 
 // 마무리 할일
@@ -75,8 +76,7 @@ class _MyTripScheduleScreenState extends State<MyTripScheduleScreen> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // 각 일정 카드들을 리스트로 반복하여 보여줍니다.
-          _upcomingScheduleCard(context),
+          _presentScheduleCard(context),
           SizedBox(height: 20), // 카드 간격
         ],
       ),
@@ -103,9 +103,8 @@ class _MyTripScheduleScreenState extends State<MyTripScheduleScreen> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          // 각 일정 카드들을 리스트로 반복하여 보여줍니다.
           _pastScheduleCard(context),
-          SizedBox(height: 20), // 카드 간격
+          SizedBox(height: 20),
         ],
       ),
     );
@@ -126,7 +125,7 @@ class _MyTripScheduleScreenState extends State<MyTripScheduleScreen> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Text('다가오는 일정'),
-                SizedBox(height: 20), // 다가오는 일정 텍스트 아래 여백 추가
+                SizedBox(height: 20),
               ],
             ),
             subtitle: Row(
@@ -162,7 +161,7 @@ class _MyTripScheduleScreenState extends State<MyTripScheduleScreen> {
               ],
             ),
             onTap: () {
-              _showImageDialog(context);
+              showTripDetailDialog(context);
             },
           ),
           Positioned(
@@ -241,7 +240,7 @@ class _MyTripScheduleScreenState extends State<MyTripScheduleScreen> {
               ],
             ),
             onTap: () {
-              _showImageDialog(context);
+              showTripDetailDialog(context);
             },
           ),
           Positioned(
@@ -320,7 +319,7 @@ class _MyTripScheduleScreenState extends State<MyTripScheduleScreen> {
               ],
             ),
             onTap: () {
-              _showImageDialog(context);
+              showTripDetailDialog(context);
             },
           ),
           Positioned(
@@ -345,55 +344,6 @@ class _MyTripScheduleScreenState extends State<MyTripScheduleScreen> {
           ),
         ],
       ),
-    );
-  }
-
-  void _showImageDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        return Dialog(
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10),
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Stack(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.all(10.0),
-                    child: Column(
-                      children: [
-                        Image.asset(
-                          '../assets/images/noImg.jpg',
-                          width: 300,
-                          height: 300,
-                          fit: BoxFit.cover,
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          '이미지가 없습니다',
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
-                  ),
-                  Positioned(
-                    right: 0,
-                    child: IconButton(
-                      icon: Icon(Icons.close),
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        );
-      },
     );
   }
 }
