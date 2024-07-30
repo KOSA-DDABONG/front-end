82e318b67db9653a470c7038cfe5c4ba60818d17
LeeZEun <je990420@ajou.ac.kr>
2024-07-30 11:31:20 +0900

    [Delete] 구글 맵 api 키 삭제

--- lib/constants.dart
+++ lib/constants.dart
@@ -4,5 +4,4 @@ const primaryColor = Colors.blue;
 const secondaryColor = Color(0xFF005AA7);
 const bgColor = Colors.white;
 
-const mapApiKey = 'AIzaSyBqFhlY9_6H7ZddukaBR7RCwzg_wYT0x1A';
-// const mapApiKey = '[YOUR API KEY]';
+const mapApiKey = '[YOUR API KEY]';
