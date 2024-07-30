5038e16131ded22f79031cd219559db741984d49
LeeZEun <je990420@ajou.ac.kr>
2024-07-30 11:02:53 +0900

    [Comment] 주석 정리

--- lib/config.dart
+++ lib/config.dart
@@ -2,8 +2,7 @@ class Config {
   static const String appName = "TripFlow";
 
   //Config
-  // static const apiURL = "ec2-43-202-92-163.ap-northeast-2.compute.amazonaws.com:8080";
-  static const apiURL = "127.0.0.1:8443";
+  static const apiURL = "localhost:8443";
 
   //User Info
   static const loginAPI = "/user/login";
--- lib/constants.dart
+++ lib/constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 const primaryColor = Colors.blue;
 const secondaryColor = Color(0xFF005AA7);
 const bgColor = Colors.white;
+
+const mapApiKey = '[YOUR API KEY]';
